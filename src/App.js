a1aa4c270a14dca1edef05d37895649eda81faea
Costan Daniel Solomon <danielcostan99@gmail.com>
2023-02-01 13:35:07 +0200

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { marked } from "marked";
+import { useState } from "react";
 
 function App() {
+  marked.setOptions({
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    smartLists: true,
+    langPrefix: "language-",
+  });
+  const [text, setText] = useState(`
+  # Heading
+
+  **This is bolded text**
+  > Block Quotes!
+
+
+  ## Heading 2
+
+  This is a paragraph
+
+  - And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+  [visit website](https://costan-daniel.com)
+
+  This is a inline  \`<div></div>\`
+
+  This is a block of code
+
+  \`\`\`
+  let x = 1;
+  let y = 2;
+  let z = x + y;
+  \`\`\`
+![React](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+ `);
+
+  const handleChange = (events) => {
+    setText(events.target.value);
+  };
+
+  const parsedMarkdown = marked(text);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="header">Convert Markdown</h1>
+
+      <div className="bodystyling">
+        <div className="row">
+          <div className="editorStyle">
+            <h4>Enter your markdown:</h4>
+            <textarea
+              className="form-control"
+              id="editor"
+              value={text}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="previewstyle">
+            <h4>Result:</h4>
+            <div
+              className="preview"
+              dangerouslySetInnerHTML={{ __html: parsedMarkdown }}
+              id="preview"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
